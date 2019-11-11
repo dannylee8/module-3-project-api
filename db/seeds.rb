651b674d267a8ae0b1d70a0530d620b720a173db
Danny Lee <dreamer@panix.com>
2019-11-11 13:48:59 -0500

    add .gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
+puts "Destroying users"
+User.all.destroy_all
+puts "Destroying all bets"
+Bet.all.destroy_all
+puts "Destroying all comments"
+Comment.all.destroy_all
+
+puts "Creating users"
+50.times do
+    User.create(
+        name: Faker::Name.first_name,
+        password: Faker::Color.name,
+        balance: rand(1..1000)
+    )
+end
+
+puts "Creating Bets"
+50.times do
+    User.create(
+        user_id: rand(1..5),
+        amount: rand(1..25),
+        result: [true, false].sample,  // randomly select true or false
+    )
+end
+
+puts "Creating Comments"
+50.times do
+    User.create(
+        user_id: rand(1..5),
+        amount: rand(1..25),
+        result: [true, false].sample,  // randomly select true or false
+    )
+end
