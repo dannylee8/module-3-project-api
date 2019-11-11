bb25c6ee43e7f2755cffd4fb37e17db282f0c212
Danny Lee <dreamer@panix.com>
2019-11-11 11:07:55 -0500

    Add relationships and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :bets 
+    has_many :comments
 end
--- db/migrate/20191111154607_create_users.rb
+++ db/migrate/20191111154607_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.float :balance
+      t.float :balance, default: 500.0
       t.float :total_profit
 
       t.timestamps
