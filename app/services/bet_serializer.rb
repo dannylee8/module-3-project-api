9680e30ec7153ea39fc5a9436152396fd04bc81a
Liam <liam.healy1@marist.edu>
2019-11-14 16:25:36 -0500

    Modified bet_serializer.rb slightly

--- app/services/bet_serializer.rb
+++ app/services/bet_serializer.rb
@@ -1,5 +1,5 @@
 class BetSerializer < ActiveModel::Serializer
-    include FastJsonapi::ObjectSerializer
+    # include FastJsonapi::ObjectSerializer
 
     attributes :user_id, :result, :amount, :created_at
     belongs_to :user
